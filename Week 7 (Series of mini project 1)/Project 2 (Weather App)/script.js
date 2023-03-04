833e06657ee654cdc1512bbf76c4813c201bcdde
LokeshSingh07 <lokeshsingh0089@gmail.com>
2023-03-05 00:33:35 +0530

    Weather App
    CSS file added , update in HTML & JS file

--- Week 7 (Series of mini project 1)/Project 2 (Weather App)/script.js
+++ Week 7 (Series of mini project 1)/Project 2 (Weather App)/script.js
@@ -10,7 +10,7 @@ const userInfoContainer = document.querySelector(`.user-info-container`);
 
 // initally variable need ??
 
-const oldTab = userTab;
+let oldTab = userTab;
 const API_KEY = "ca0a23447cb3e73e5baa8885ff2eddda";
 oldTab.classList.add("current-tab");
 
@@ -108,14 +108,14 @@ function renderWeatherInfo(weatherInfo){
     const cloudiness = document.querySelector("[data-cloudiness]");
 
 
-    weatherIcon.inerText = weatherInfo?.name;
-    countryIcon.src = `https://flagcdn.com/144×108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
-    desc.inerText = weatherIcon?.weather?.[0]?.description;
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    temp.innerText = weatherInfo?.main?.temp;
-    windSpeed.innerText = weatherInfo?.wind?.speed;
-    humidity.innerText = weatherInfo?.main?.humidity;
-    cloudiness.innerText = weatherInfo?.clouds?.all;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windSpeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
 
 }
 
