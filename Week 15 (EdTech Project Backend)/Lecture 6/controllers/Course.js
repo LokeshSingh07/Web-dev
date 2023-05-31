bc4b7f9183835830c01a2fa80435c669030ca41f
LokeshSingh07 <lokeshsingh0089@gmail.com>
2023-05-31 15:54:11 +0530

    EdTech Project BD - L6

---
+++ Week 15 (EdTech Project Backend)/Lecture 6/controllers/Course.js
@@ -0,0 +1,201 @@
+const Course = require("../models/CourseModel");
+const Category = require("../models/CategoryModel");
+const User =require("../models/UserModel");
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
+
+
+
+
+// createCourse handler function
+exports.createCourse = async(req,res)=>{
+    try{
+        // fetch data from req body
+        const {courseName, courseDescription, whatYouWillLearn, price, category, tag, status, instructions} = req.body;
+
+        // fetch file
+        const thumbnail = req.files.thumbnailImage;
+        
+        // Validation
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !category || !tag || !thumbnail){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
+        if(!status || status === undefined){
+            status = 'Draft'
+        }
+        
+        // Instructor validation --> Check if the user is an instructor
+        const instructorDetails = await User.findById(userId, {
+            accountType: "Instructor",
+        });
+        console.log("Instructor details : ", instructorDetails);
+        
+        if(!instructorDetails){
+            return res.status(404).json({
+                success: false,
+                message: "Instructor details Not Found",
+            });
+        }
+
+        // Check given Category is valid or not 
+        const categoryDetails = await Category.findById(category);
+        if(!categoryDetails){
+            return res.status(404).json({
+                success: false,
+                message: "Category details Not Found",
+            });
+        }
+        
+        // Image --> clodinary
+        const thumbnailImage = await uploadImageToCloudinary(thumbnailImage, process.env.FOLDER_NAME);
+        
+        // create an entry for new course 
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+            whatYouWillLearn: whatYouWillLearn,
+            price,
+            tag: tag,
+            category: categoryDetails._id,
+            thumbnail: thumbnailImage.secure_url,
+            status: status,
+            instructions: instructions,
+        });
+        
+        
+        // add the new course to the User Schema of Instructor
+        await User.findByIdAndUpdate(
+            {_id: instructorDetails._id},
+            {
+                $push: {
+                    courses: newCourse._id,
+                }
+            },
+            {new: true}
+        );
+        
+        // TODO [HW] --> Add course entry in Category Schema
+        // const categoryId = await findById(category);
+        await Category.findByIdAndUpdate(
+            {_id: categoryDetails._id},
+            {
+                $push: {
+                    Course: newCourse._id
+                }
+            },
+            {new: true}
+        )
+
+        
+        // return the new course and a success response
+        return res.status(200).json({
+            success: true,
+            message: "Course created successfully",
+            data: newCourse,
+        })
+
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to create course",
+            error: err.message,
+        });
+    }
+}
+
+
+
+
+
+
+
+// getAllCourse handler function
+exports.getAllCourses = async (req,res)=>{
+    try{
+        const allCourses = await Course.find({},
+            {
+                courseName: true,
+                price: true,
+                thumbnail: true,
+                instructor: true,
+                ratingAndReviews: true,
+                studentsEnrolled: true,
+            }
+        ).populate("Instructor").exec();
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "Data for all courses fetched successfully",
+            data: allCourses,
+        });
+
+
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Cannot fetch course data",
+            error: err.message,
+        });
+    }
+}
+
+
+
+
+
+
+// getCourseDetails handler function
+exports.getCourseDetails = async(req,res)=>{
+    try{
+        // get id
+        const {courseId} = req.body;
+
+        // find course details
+        const courseDetails = await Course.find({_id: courseId})
+                                                .populate({
+                                                    path: "instructor",
+                                                    populate: {
+                                                        path: "additionalDetails"
+                                                    }
+                                                })
+                                                .populate({
+                                                    path: "courseContent",
+                                                    populate: {
+                                                        path: "subSection"
+                                                    }
+                                                })
+                                                .populate("ratingAndReviews")
+                                                .populate("category")
+                                                exec();
+
+        // validation
+        if(!courseDetails){
+            return res.status(400).json({
+                success: false,
+                message: `Could not find the course with ${courseId}`,
+            });
+        }
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: `Course details fetched successfully`,
+            data: courseDetails,
+        });
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+    }
+}
+
---
+++ Week 15 (EdTech Project Backend)/Lecture 6/controllers/ResetPassword.js
@@ -0,0 +1,119 @@
+const User = require("../models/UserModel");
+const mailSender = require("../utils/MailSender");
+const bcrypt = require("bcrypt");
+
+
+// Reset password token
+exports.resetPasswordToken = async(req,res)=>{
+    try{
+        // get email from request body
+        const {email} = req.body;
+
+        // check user for this email , email validation
+        const user = await User.findOne({email: email});
+        if(!user){
+            return res.json({
+                success: false,
+                message: `This Email: ${email} is not Registered with us. Enter a valid Email id `,
+            });
+        }
+
+        // generate Token
+        const token = crypto.randomBytes(20).toString("hex");
+
+        // update user by adding token and expiration time
+        const updatedDetails = await User.findOneAndUpdate({email: email}, 
+                                            {
+                                                token: token,
+                                                resetPasswodExpires: Date.now() + 5*60*1000,
+                                            }, 
+                                            {new: true});
+        console.log(("Details: ", updatedDetails));
+
+        // create url
+        const url = `http://localhost:3000/update-password/${token}`;
+
+
+        // send email containing the url    
+        await mailSender(email, "Password reset link", `Password Reset link : ${url}. Please click this url to reset your password`);
+
+        // return res
+        res.status(200).json({
+            success: true,
+            message: "Email sent successfully, Please check email and change password",
+        })
+
+
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong while sending reset password",
+        });
+    }
+}
+
+
+
+// ResetPassword handler function
+exports.resetPassword = async(req, res)=>{
+    try{
+        // fetch data from request body
+        const {password, confirmPassword, token} = req.body;
+    
+        // validation
+        if(password !== confirmPassword){
+            return res.status(400).json({
+                success: false,
+                message : "Password and confirm Password does not matching",
+            });
+        }
+    
+        // get user details from DB using token
+        const userDetails = await User.findOne({token: token});
+    
+        // if no entry - Invalid token
+        if(!userDetails){
+            return res.status(401).json({
+                success: false,
+                message : "Token is Invalid",
+            })
+        }
+    
+        // check token expiration time
+        if(userDetails.resetPasswordExpires < Date.now()){
+            return res.status(403).json({
+                success:  false,
+                message: "Token is Expired, Please regenerate your token",
+            });
+        }
+    
+    
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        // Update the password
+        await User.findOneAndUpdate(
+            {token : token},
+            {password : hashedPassword},
+            {new: true},
+        );
+    
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "Password reset Successfully",
+        })
+            
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong while reset password",
+        });
+    }
+}
+
+
